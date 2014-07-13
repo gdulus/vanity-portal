ca04f77a6478e5c51c6e77badde21b30ed1e622a
pawel.gdula <gdula.pawel@gmail.com>
2014-07-13 16:49:53 +0200

    - more logging

--- grails-app/controllers/vanity/portal/result/ResultController.groovy
+++ grails-app/controllers/vanity/portal/result/ResultController.groovy
@@ -24,7 +24,7 @@ class ResultController {
 
         String url = createLink(action: 'showPreview', absolute: true, params: [id: id, title: article.title.encodeAsPrettyUrl()])
         log.info('Redirecting request by id {} to full url {} (serverURL = {})', id, url, serverURL)
-        redirect(url: url)
+        redirect(url: url.replaceAll('//news/', '/news/'))
     }
 
     def showPreview(final Long id) {
